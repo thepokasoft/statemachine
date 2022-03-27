24de0eb28e79d45ba1f08f0bf75d923d7bede1f2
thepokasoft <pokasoft9@gmail.com>
2022-03-26 20:13:08 -0400

    initial Commit

---
+++ OrderStateMachine/src/main/java/com/example/demo/OrderStateMachineApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderStateMachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderStateMachineApplication.class, args);
+	}
+
+}
